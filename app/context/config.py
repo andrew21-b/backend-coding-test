cfc9afe3dfbdf53bde0beae541d224177b0ff2e9
Andrew Boateng <andrew.21@outlook.com>
2025-07-27 02:50:17 +0100

    Updates config.py to use .env for database password

--- app/context/config.py
+++ app/context/config.py
@@ -3,7 +3,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Customer API"
     VERSION: str = "1.0"
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/intropy-test"
+    DATABASE_PASSWORD: str
 
+    class Config:
+        env_file = ".env"
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql://postgres:{self.DATABASE_PASSWORD}@localhost:5432/intropy-test"
 
 settings = Settings()
